da9fae51bce86556050ebf3646ab40a639fc6117
Lari Oncescu <larioncescu@yahoo.com>
2022-01-08 14:30:36 +0200

    first subpoint

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ const poll = {
   options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const userInput = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\nWrite option number`
+      )
+    )
+  },
 }
